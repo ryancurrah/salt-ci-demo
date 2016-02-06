172db48053ce7143398d25690862c12c0664b11d
Ryan Currah <ryan@currah.ca>
2016-02-05 23:34:24 -0500

    Update test_pillar_render.py
    renamed r to pillar

--- tests/pytest/test_pillar_render.py
+++ tests/pytest/test_pillar_render.py
@@ -3,7 +3,7 @@ caller = salt.client.Caller()
 
 
 def test_pillar_render():
-    r = caller.cmd('pillar.items')
-    assert isinstance(r, dict)
-    assert '_errors' not in r
+    pillar = caller.cmd('pillar.items')
+    assert isinstance(pillar, dict)
+    assert '_errors' not in pillar
 
